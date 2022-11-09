b2408930cb7d2d8fa5dd8660af8ea95f3080ad23
Vlad <vlad.bashtannyk11@gmail.com>
2022-11-09 12:47:59 +0200

    Added prices as DecimalField

--- store/serializers.py
+++ store/serializers.py
@@ -4,6 +4,8 @@ from store.models import Product, ShoppingCartItem
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+    quantity = serializers.IntegerField(min_value=1, max_value=100)
+
     class Meta:
         model = ShoppingCartItem
         fields = ["product", "quantity"]
@@ -14,6 +16,10 @@ class ProductSerializer(serializers.ModelSerializer):
     current_price = serializers.FloatField(read_only=True)
     description = serializers.CharField(min_length=2, max_length=200)
     cart_items = serializers.SerializerMethodField()
+    price = serializers.DecimalField(
+        min_value=1, max_value=10000,
+        max_digits=None, decimal_places=2,
+    )
 
     class Meta:
         model = Product
